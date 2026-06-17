595fe1b3654d5a2020cf8b98ec84fb64ce87f200
keviin10 <rahme.kevin@hotmail.com>
2026-06-17 08:31:43 +0300

    updated index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-// Store scroll positions
-const scrollStore = new Map()
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/HomeView.vue')
+    component: () => import('@/views/HomeView.vue'),
   },
   {
     path: '/collections',
     name: 'Collections',
-    component: () => import('@/views/CollectionsView.vue')
+    component: () => import('@/views/CollectionsView.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('@/views/AboutView.vue')
+    component: () => import('@/views/AboutView.vue'),
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('@/views/ContactView.vue')
+    component: () => import('@/views/ContactView.vue'),
   },
   {
     path: '/reviews',
     name: 'Reviews',
-    component: () => import('@/views/ReviewsView.vue')
+    component: () => import('@/views/ReviewsView.vue'),
   },
   {
     path: '/cart',
     name: 'Cart',
-    component: () => import('@/views/CartView.vue')
+    component: () => import('@/views/CartView.vue'),
   },
 
   {
-  path: '/wishlist',
-  name: 'Wishlist',
-  component: () => import('@/views/WishlistView.vue')
+    path: '/wishlist',
+    name: 'Wishlist',
+    component: () => import('@/views/WishlistView.vue'),
   },
   {
     path: '/:category',
     name: 'Category',
     component: () => import('@/views/CategoryView.vue'),
-    props: true
+    props: true,
   },
   {
     path: '/product/:id',
     name: 'ProductDetail',
-    component: () => import('@/views/ProductDetailView.vue')
+    component: () => import('@/views/ProductDetailView.vue'),
   },
-   {
-  path: '/checkout',
-  name: 'Checkout',
-  component: () => import('@/views/CheckoutView.vue')
-  }
-]
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import('@/views/CheckoutView.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
@@ -66,39 +63,13 @@ const router = createRouter({
     if (savedPosition) {
       return new Promise((resolve) => {
         setTimeout(() => {
-          resolve(savedPosition)
-        }, 150)
-      })
+          resolve(savedPosition);
+        }, 150);
+      });
     }
-    
-    // Check if we have a stored position for this route
-    const storedPosition = scrollStore.get(to.fullPath)
-    if (storedPosition) {
-      return storedPosition
-    }
-    
-    // Check if we're coming back to a previous page
-    if (from.name && to.name !== from.name) {
-      // Save the current position before leaving
-      scrollStore.set(from.fullPath, {
-        top: window.pageYOffset,
-        left: window.pageXOffset
-      })
-    }
-    
-    return { top: 0 }
-  }
-})
+    // Always scroll to top for normal navigations
+    return { top: 0 };
+  },
+});
 
-// Save scroll position before navigation
-router.beforeEach((to, from, next) => {
-  if (from.path && from.path !== to.path) {
-    scrollStore.set(from.fullPath, {
-      top: window.pageYOffset,
-      left: window.pageXOffset
-    })
-  }
-  next()
-})
-
-export default router
+export default router;
